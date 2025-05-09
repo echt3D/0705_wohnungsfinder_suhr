45ee6e3b5dea9165d06cbda6a799917e9a1bdde6
arisa-echt3d <arisa@echt3d.ch>
2025-05-09 11:27:31 +0200

    changed gemietet to vermietet

--- app/components/StatusBatch.tsx
+++ app/components/StatusBatch.tsx
@@ -2,7 +2,7 @@ const StatusBatch = ({ status }: { status: string }) => {
   return (
     <div
       className={`px-2 py-1 bg-status_badge rounded-md  text-sm text-white ${
-        status === "frei" ? "opacity-100" : "opacity-80"
+        status === "frei" ? "bg-opacity-100" : "bg-opacity-50"
       }`}
     >
       <span>{status}</span>
